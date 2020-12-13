4b68ea8420418306119aae21d0ab399b2e45ae28
Rui-M1 <40121317+Rui-M1@users.noreply.github.com>
2020-12-13 23:17:25 +0000

    Add files via upload

--- atrabalhar.c
+++ atrabalhar.c
@@ -7,80 +7,83 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/wait.h>
 
 int distance(int size, int path[size], int matrix[size][size])
 {
-	int dist = 0;
-	for(int i = 0; i < size - 1; i++)
-	{
-		int curr = path[i] - 1;
-		int next = path[i + 1] - 1;
-		dist += matrix[curr][next];
-	}
-	int last = path[size - 1] - 1;
-	int first = path[0] - 1;
-	dist += matrix[last][first];
-	return dist;
+    int dist = 0;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int curr = path[i] - 1;
+        int next = path[i + 1] - 1;
+        dist += matrix[curr][next];
+    }
+    int last = path[size - 1] - 1;
+    int first = path[0] - 1;
+    dist += matrix[last][first];
+    return dist;
 }
 
 void swap(int size, int path[size])
 {
-	int a = rand() % size;
-	int b = rand() % size;
-	int tmp = path[a];
-	path[a] = path[b];
-	path[b] = tmp;
+    int a = rand() % size;
+    int b = rand() % size;
+    int tmp = path[a];
+    path[a] = path[b];
+    path[b] = tmp;
 }
 
-int* createShmem(int size)
+int *createShmem(int size)
 {
-	int protection = PROT_READ | PROT_WRITE;
-	int visibility = MAP_SHARED | MAP_ANONYMOUS;
-	return mmap(NULL, sizeof(int) * size, protection, visibility, 0, 0);
+    int protection = PROT_READ | PROT_WRITE;
+    int visibility = MAP_SHARED | MAP_ANONYMOUS;
+    return mmap(NULL, sizeof(int) * size, protection, visibility, 0, 0);
 }
 
 int main()
 {
-	int dist1 = 0;
-	int dist2 = 0;
+    int dist1 = 0;
+    int dist2 = 0;
+    int i = 0;
 
-	sem_unlink("openMem");
-	sem_t *sem1 = sem_open("openMem", O_CREAT, 0644, 0);
+    sem_unlink("openMem");
+    sem_unlink("closeMem");
+    sem_t *sem1 = sem_open("openMem", O_CREAT, 0644, 1);
+    sem_t *sem2 = sem_open("closeMem", O_CREAT, 0644, 0);
 
-	int* shmem = createShmem(64);
+    int *shmem = createShmem(64);
 
-	int numProcess;
-	printf("Introduza o número de processos que deseja criar: ");
-	scanf("%d", &numProcess);
+    int numProcess;
+    printf("Introduza o número de processos que deseja criar: ");
+    scanf("%d", &numProcess);
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	int size = 5;
+    int size = 5;
 
-	int matrix[5][5] =
-       	{
-		{0, 23, 10, 4, 1},
-		{23, 0, 9, 5, 4},
-		{10, 9, 0, 8, 2},
-		{4, 5, 8, 0, 11},
-		{1, 4, 2, 11, 0},
-	};
+    int matrix[5][5] =
+        {
+            {0, 23, 10, 4, 1},
+            {23, 0, 9, 5, 4},
+            {10, 9, 0, 8, 2},
+            {4, 5, 8, 0, 11},
+            {1, 4, 2, 11, 0},
+        };
 
-	int path[] = {1, 2, 3, 4, 5};
+    int path[] = {1, 2, 3, 4, 5};
 
-	    for(int i = 0; i < numProcess; i++)
+    int pid = fork();
+    char num[258];
+
+    for (int i = 0; i < numProcess; i++)
     {
-        int pid = fork();
-        char num[258];
 
-        //sem_wait(sem1);
-        if(pid == 0)
+        if (pid == 0)
         {
             swap(size, path);
             dist1 = distance(size, path, matrix);
-
             sem_wait(sem1);
-            if(shmem[0] == 0  shmem[0] > dist1)
+            if (shmem[0] == 0 || shmem[0] > dist1)
                 shmem[0] = dist1;
             sem_post(sem2);
         }
@@ -88,36 +91,18 @@ int main()
         {
             swap(size, path);
             dist2 = distance(size, path, matrix);
-
             sem_wait(sem2);
-            if(shmem[0] == 0  shmem[0] > dist2)
+            if (shmem[0] == 0 || shmem[0] > dist2)
                 shmem[0] = dist2;
             sem_post(sem1);
         }
-        //sem_post(sem2);
-
-        /*
-        sem_wait(sem2);
-        if(shmem[0] == 0)
-        {
-            if(dist1 > dist2)
-                shmem[0] = dist2;
-            else 
-                shmem[0] = dist1;
-        }
-        else
-        {
-            if(dist1 > dist2 && shmem[0] > dist2 && shmem[0] > dist1) 
-                shmem[0] = dist2;
-
-            else if(dist1 < dist2 && shmem[0] > dist2 && shmem[0] > dist1) 
-                shmem[0] = dist1;
-        }
-        sem_post(sem1);
-        */
     }
+    sem_close(sem1);
+    sem_close(sem2);
 
-	sem_close(sem1);
+    printf("Distancia: %d\n", shmem[0]);
+    sleep(2);
 
-	return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
+    
 }
